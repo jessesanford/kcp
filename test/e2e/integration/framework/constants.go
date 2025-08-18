6848e85f5e60736df42cc70c34c4acabf737275d
Jesse Sanford <108698+jessesanford@users.noreply.github.com>
2025-08-18 08:01:21 +0000

    refactor(constants): simplify constants file to reduce lines
    - Combine const blocks and remove excessive comments
    - Maintain all functionality while reducing file size
    
    Signed-off-by: Jesse Sanford <108698+jessesanford@users.noreply.github.com>

--- test/e2e/integration/framework/constants.go
+++ test/e2e/integration/framework/constants.go
@@ -19,108 +19,42 @@ package framework
 import "time"
 
 const (
-	// Test environment constants
+	// Test defaults
+	DefaultTestPrefix         = "it-"
+	DefaultTestPortBase       = 30100
+	IntegrationTestNamespace  = "integration-tests"
 	
-	// DefaultTestPrefix is the default prefix for test resource names
-	DefaultTestPrefix = "it-"
+	// Timeouts and polling
+	TestTimeout                    = 5 * time.Minute
+	TestPollInterval               = 2 * time.Second
+	ResourceReadyTimeout           = 5 * time.Minute
+	ResourceReadyPollInterval      = 2 * time.Second
+	ResourceDeletionTimeout        = 2 * time.Minute
+	ResourceDeletionPollInterval   = 2 * time.Second
 	
-	// DefaultTestPortBase is the default base port for test services
-	DefaultTestPortBase = 30100
-	
-	// IntegrationTestNamespace is the dedicated namespace for TMC integration tests
-	IntegrationTestNamespace = "integration-tests"
-)
-
-const (
-	// Timeout and polling constants
-	
-	// TestTimeout is the default timeout for integration test operations
-	TestTimeout = 5 * time.Minute
-	
-	// TestPollInterval is the default polling interval for integration tests
-	TestPollInterval = 2 * time.Second
-	
-	// ResourceReadyTimeout is the timeout for waiting for resources to become ready
-	ResourceReadyTimeout = 5 * time.Minute
-	
-	// ResourceReadyPollInterval is the polling interval for resource ready checks
-	ResourceReadyPollInterval = 2 * time.Second
-	
-	// ResourceDeletionTimeout is the timeout for waiting for resource deletion
-	ResourceDeletionTimeout = 2 * time.Minute
-	
-	// ResourceDeletionPollInterval is the polling interval for resource deletion checks
-	ResourceDeletionPollInterval = 2 * time.Second
-)
-
-const (
-	// Standard test labels and annotations
-	
-	// TestSuiteLabelKey is the key for the test suite label
-	TestSuiteLabelKey = "test-suite"
-	
-	// TestSuiteLabelValue is the value for the TMC integration test suite label
-	TestSuiteLabelValue = "tmc-integration"
-	
-	// TestRunLabelKey is the key for the test run label
-	TestRunLabelKey = "test-run"
-	
-	// TestWorkspaceLabelKey is the key for the test workspace label
-	TestWorkspaceLabelKey = "test-workspace"
-	
-	// TestNamespaceLabelKey is the key for the test namespace label
-	TestNamespaceLabelKey = "test-namespace"
-	
-	// TestTypeLabelKey is the key for the test type label
-	TestTypeLabelKey = "test-type"
-	
-	// TestNameLabelKey is the key for the test name label
-	TestNameLabelKey = "test-name"
-	
-	// ManagedByLabelKey is the key for the managed-by label
-	ManagedByLabelKey = "managed-by"
-	
-	// ManagedByLabelValue is the value for the managed-by label for TMC integration tests
-	ManagedByLabelValue = "tmc-integration-tests"
-	
-	// CleanupPolicyLabelKey is the key for the cleanup policy label
-	CleanupPolicyLabelKey = "cleanup-policy"
-	
-	// AutomaticCleanupPolicyValue is the value for automatic cleanup policy
+	// Standard labels and annotations
+	TestSuiteLabelKey         = "test-suite"
+	TestSuiteLabelValue       = "tmc-integration"
+	TestRunLabelKey           = "test-run"
+	TestWorkspaceLabelKey     = "test-workspace"
+	TestNamespaceLabelKey     = "test-namespace"
+	TestTypeLabelKey          = "test-type"
+	TestNameLabelKey          = "test-name"
+	ManagedByLabelKey         = "managed-by"
+	ManagedByLabelValue       = "tmc-integration-tests"
+	CleanupPolicyLabelKey     = "cleanup-policy"
 	AutomaticCleanupPolicyValue = "automatic"
+	TestNameAnnotationKey     = "test.kcp.io/test-name"
+	TestCreatedAnnotationKey  = "test.kcp.io/created"
 	
-	// TestNameAnnotationKey is the key for the test name annotation
-	TestNameAnnotationKey = "test.kcp.io/test-name"
-	
-	// TestCreatedAnnotationKey is the key for the test creation time annotation
-	TestCreatedAnnotationKey = "test.kcp.io/created"
-)
-
-const (
-	// Workspace and resource constants
-	
-	// UniversalWorkspaceType is the workspace type for universal workspaces
+	// Workspace constants
 	UniversalWorkspaceType = "universal"
-	
-	// RootWorkspacePath is the path for the root workspace
-	RootWorkspacePath = "root"
-	
-	// DefaultParentWorkspace is the default parent workspace for test workspaces
+	RootWorkspacePath      = "root"
 	DefaultParentWorkspace = "root:default"
-)
-
-const (
-	// Condition types and statuses
 	
-	// ReadyConditionType is the condition type for readiness
-	ReadyConditionType = "Ready"
-	
-	// TrueConditionStatus is the condition status for true
-	TrueConditionStatus = "True"
-	
-	// FalseConditionStatus is the condition status for false
-	FalseConditionStatus = "False"
-	
-	// UnknownConditionStatus is the condition status for unknown
+	// Condition constants
+	ReadyConditionType     = "Ready"
+	TrueConditionStatus    = "True"
+	FalseConditionStatus   = "False"
 	UnknownConditionStatus = "Unknown"
 )
