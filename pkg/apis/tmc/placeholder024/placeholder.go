2f5b0e4fa696940398022a3ccc6bc79163083579
TMC Integration <tmc@kcp.io>
2025-08-19 06:11:26 +0000

    feat(api): add TMC API placeholder component 024
    Placeholder implementation for PR staging branch 024.
    Part of TMC implementation Wave 4.
    
    Signed-off-by: TMC Integration <tmc@kcp.io>

---
+++ pkg/apis/tmc/placeholder024/placeholder.go
@@ -0,0 +1,12 @@
+// TODO: Implement TMC API component 024
+package placeholder024
+
+// Placeholder024 is a placeholder for PR staging
+type Placeholder024 struct {
+    Name string `json:"name"`
+}
+
+// NewPlaceholder024 creates a new placeholder
+func NewPlaceholder024() *Placeholder024 {
+    return &Placeholder024{Name: "placeholder-024"}
+}
